d626f6024ff3d5c79a800f8b88933be9a8933452
unidash <dasha.podkuyko@mail.ru>
2019-01-20 15:19:56 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("second commit");
+        System.out.println("next commit");
     }
 }
